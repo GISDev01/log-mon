3539f06b31002a305bd5702645475db2e0441fa9
gisdev01 <gisdev01@gmail.com>
2018-02-01 20:07:27 -0500

    Rename the CSV log that gets loaded into PG

--- logparser.py
+++ logparser.py
@@ -2,6 +2,7 @@ import logging
 import os
 import json
 import csv
+import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -42,7 +43,14 @@ def get_config_options(config_filepath):
 sql_session = sessionmaker(bind=db_engine)
 SQLAlchBaseClass.metadata.create_all(db_engine)
 
-with open(CSV_FILEPATH, 'r') as csvfile:
+local_datetime = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
+
+new_csv_filename = CSV_FILEPATH[:-4] + '_{}.log'.format(local_datetime)
+
+# Assuming this database operation will go ok, we can reset the log back to scratch and keep the old one around
+os.rename(CSV_FILEPATH, new_csv_filename)
+
+with open(new_csv_filename, 'r') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     skipped_lines = 0
     while skipped_lines < NUM_HEADER_LINES_TO_SKIP:
@@ -54,7 +62,7 @@ with open(CSV_FILEPATH, 'r') as csvfile:
 # Create the table in postgres using the Class definition if it's not created already
 SQLAlchBaseClass.metadata.create_all(db_engine)
 
-# for record in records_to_insert:
-#     logger.info(record.StartDateTime)
-# session.add_all(records_to_insert)
-# session.commit()
+for record in records_to_insert:
+    logger.info(record.StartDateTime)
+session.add_all(records_to_insert)
+session.commit()
